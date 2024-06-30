7859f562c05ced12e97b5f66a50df019090cf4a1
skrotova <skrotova@kse.org.ua>
2024-06-30 21:07:52 +0300

    added file to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv')
+const webpack = require('webpack')
+
+const localEnv = dotenv.config().parsed
+
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,7 +12,38 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          // Replaces existing CSS rules with given rule
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  esModule: false,
+                  modules: {
+                    localIdentName: '[local]--[name]--[hash:base64:5]'
+                  }
+                }
+              }
+            ]
+          }
+        ],
+        plugins: [
+          new webpack.DefinePlugin({
+            'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+            'process.env.MAP_KEY': localEnv.MAP_KEY
+              ? JSON.stringify(localEnv.MAP_KEY)
+              : JSON.stringify(process.env.MAP_KEY)
+          })
+        ]
+      }
+    }
   ],
   framework: {
     name: '@storybook/react-webpack5',
