d70fcc6ddcf7cb713633b9bc465c4a923cc5d4ee
skrotova <skrotova@kse.org.ua>
2024-06-30 20:47:27 +0300

    installed storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,30 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true
-      }
-    },
-    env: {
-      browser: true,
-      es2021: true
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['react', 'react-hooks'],
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/no-unescaped-entities': 0
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended'
+  ],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 0
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
   }
+}
